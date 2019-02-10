ca6eafa51fd637eb20f219b0744bf37b03def4da
diescake <daisuke.kondo@access-company.com>
2019-02-10 22:33:00 +0900

    Implement Todo logics

--- src/app/components/Todo/index.tsx
+++ src/app/components/Todo/index.tsx
@@ -1,5 +1,6 @@
 import words from '@/assets/strings'
 import * as React from 'react'
+import { useState } from 'react'
 import * as key from 'weak-key'
 import style from './style.scss'
 
@@ -17,10 +18,15 @@ const initialTodos: TODO[] = [
     message: 'Read and learn React Hooks document.',
     done: false,
   },
+  {
+    message: 'Sleep for 14 hours.',
+    done: false,
+  },
 ]
 
 export default () => {
-  const todos = initialTodos
+  const [todos, setTodos] = useState<TODO[]>(initialTodos)
+  const [todoMessage, setTodoMessage] = useState<string>('')
 
   return (
     <div className={style.container}>
@@ -30,8 +36,13 @@ export default () => {
           <li key={key(todo)}>{todo.message}</li>
         ))}
       </ul>
-      <input onChange={() => console.log('changed')} type="text" placeholder="Enter your new TODO !!" />
-      <button onClick={() => console.log('clicked')}>{words.todo.add}</button>
+      <input
+        onChange={(e: any) => setTodoMessage(e.target.value)}
+        type="text"
+        placeholder="Enter new TODO message."
+        value={todoMessage}
+      />
+      <button onClick={() => setTodos([...todos, { message: todoMessage, done: false }])}>{words.todo.add}</button>
     </div>
   )
 }
